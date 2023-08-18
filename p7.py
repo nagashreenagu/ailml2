07183345166d914964b31af1b570f42b5b7d5712
nagashree <nagashree@gmail.com>
2023-08-18 00:07:54 -0700

    change7

---
+++ p7.py
@@ -0,0 +1,11 @@
+l1=[1,1,1,1]
+l2=[1,2,3,4]
+l3=[1,1,1,1]
+l=list(map(lambda x,y,z:x+y+z,l1,l2,l3))
+print(l)
+
+fruits =["mango","apple","orange","kiwi"]
+d=list(filter(lambda x:'g'in x,fruits))
+print(d)
+
+
