3ff64d30c7a54bd8546872340079507175cf4f1b
nagashree <nagashree@gmail.com>
2023-08-18 00:10:26 -0700

    change1

---
+++ p1.py
@@ -0,0 +1,7 @@
+import numpy as np
+arr = np.array([[1,2,3],[4,2,5]])
+print("array is of type:",type(arr))
+print("number of dimention:",arr.ndim)
+print("shape of array:",arr.shape)
+print("size of array:",arr.size)
+print("array stores element of type:",arr.dtype) 
